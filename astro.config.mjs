ae24c9fa14e1a99409af4d6ea67bdc11850cafb0
Bryan Robinson <bryanlrobinson@gmail.com>
2023-01-17 09:48:12 -0500

    Removes solid dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import solidJs from "@astrojs/solid-js";
-
-// https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs(), react()]
+  integrations: [tailwind(), react()]
 });
